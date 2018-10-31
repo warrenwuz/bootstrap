3468376ee924f4f6d43d7e9380f636ddefa01fa5
吴哲 <wuzhe1159@163.com>
2018-10-31 08:54:14 +0800

    init

---
+++ src/main/java/com/warren/bootstrap/BootstrapApplication.java
@@ -0,0 +1,17 @@
+package com.warren.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuzhe1159@163.com
+ * @date
+ * @since jdk11
+ */
+@SpringBootApplication
+public class BootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapApplication.class, args);
+    }
+}
---
+++ src/test/java/com/warren/bootstrap/BootstrapApplicationTests.java
@@ -0,0 +1,26 @@
+package com.warren.bootstrap;
+
+import com.warren.bootstrap.knights.BraveKnight;
+import com.warren.bootstrap.knights.Quest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootstrapApplicationTests {
+
+    @Test
+    public void contextLoads() {
+		Quest mockQuest = mock(Quest.class);
+		BraveKnight knight = new BraveKnight(mockQuest);
+		knight.embarkOnQuest();
+		verify(mockQuest, times(1)).embark();
+    }
+
+}
